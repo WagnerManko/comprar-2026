68eb1c79c044710a56dd6a891b9444ecd273d5fb
Wagner Manko <wagner.manko@gmail.com>
2026-04-27 13:11:10 -0300

    first commit

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,16 @@
+import { Text, TouchableOpacity, TouchableOpacityProps } from "react-native"
+import { styles } from "./styles"
+
+interface ButtonProps extends TouchableOpacityProps {
+    title: string;
+}
+
+export const Button = ({title, ...rest}: ButtonProps) => {
+    return (
+        <TouchableOpacity style={styles.container} activeOpacity={0.8} {...rest}>
+            <Text style={styles.text}>
+                {title}
+            </Text>
+        </TouchableOpacity>
+    )
+}
---
+++ src/components/Filter/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+  },
+  title: {
+    fontSize: 12,
+    fontWeight: 600,
+  },
+});
